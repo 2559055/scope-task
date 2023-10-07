a49a929e7ea043ae83d0319613e6fd1f326dfc1a
user.name <mohamed.hussein@student.moringaschool.com>
2023-10-07 08:03:35 -0700

    task of scope

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+const customerName = "zaki";
+function getcustomerName(){
+    return customerName;
+}
+console.log(getcustomerName(customerName));
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+  
+  // Function to set the best customer
+  function setBestCustomer() {
+    bestCustomer = "Alice"; // This variable is declared in the global scope
+  }
+  
+  // Function to overwrite the best customer
+  function overwriteBestCustomer() {
+    bestCustomer = "Eve"; // This will overwrite the value of bestCustomer
+  }
+  
+  // Function to unsuccessfully reassign the least favorite customer
+  function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = "John";
+    leastFavoriteCustomer = "Jane"; 
+  }
+  
+  
+  console.log(returnCustomerName()); 
+  upperCaseCustomerName(); 
+  
+  setBestCustomer(); 
+  console.log(bestCustomer); 
+  overwriteBestCustomer(); 
+  console.log(bestCustomer); 
+  changeLeastFavoriteCustomer(); 
+
